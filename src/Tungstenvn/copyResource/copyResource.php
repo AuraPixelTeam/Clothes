fd7f20fb173abfdf2ec2ba680e26209263e33875
TungstenVn <40385888+TungstenVn@users.noreply.github.com>
2020-03-12 11:06:32 +0700

    Useable

---
+++ src/Tungstenvn/copyResource/copyResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace TungstenVn\copyResource;
+
+use pocketmine\Player; 
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Event;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\entity\Skin;
+use TungstenVn\Main;
+class copyResource {
+
+	public function recurse_copy($src,$dst) { 
+        $dir = opendir($src); 
+        @mkdir($dst); 
+        while(false !== ( $file = readdir($dir)) ) { 
+            if (( $file != '.' ) && ( $file != '..' )) { 
+                if ( is_dir($src . '/' . $file) ) { 
+                    $this->recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+                } 
+                else { 
+                    copy($src . '/' . $file,$dst . '/' . $file); 
+                } 
+            } 
+        } 
+        closedir($dir); 
+    }
+}
