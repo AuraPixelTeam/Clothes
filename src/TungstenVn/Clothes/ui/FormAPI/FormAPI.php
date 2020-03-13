02258789cae5d8d0f6d7b9e5f63b97a3a12f073c
TungstenVn <huutung2003@gmail.com>
2020-03-13 09:24:27 +0700

    done

--- src/TungstenVn/Clothes/ui/FormAPI/FormAPI.php
+++ src/TungstenVn/Clothes/ui/FormAPI/FormAPI.php
@@ -4,36 +4,17 @@ declare(strict_types = 1);
 
 namespace TungstenVn\Clothes\ui\FormAPI;
 
-use pocketmine\plugin\PluginBase;
-
+use TungstenVn\Clothes\libs\jojoe77777\FormAPI\SimpleForm;
+use TungstenVn\Clothes\libsjojoe77777\FormAPI\CustomForm;
+use TungstenVn\Clothes\libsjojoe77777\FormAPI\ModelForm;
 class FormAPI{
 
-    /**
-     * @deprecated
-     *
-     * @param callable|null $function
-     * @return CustomForm
-     */
     public function createCustomForm(?callable $function = null) : CustomForm {
         return new CustomForm($function);
     }
-
-    /**
-     * @deprecated
-     *
-     * @param callable|null $function
-     * @return SimpleForm
-     */
     public function createSimpleForm(?callable $function = null) : SimpleForm {
         return new SimpleForm($function);
     }
-
-    /**
-     * @deprecated
-     *
-     * @param callable|null $function
-     * @return ModalForm
-     */
     public function createModalForm(?callable $function = null) : ModalForm {
         return new ModalForm($function);
     }
